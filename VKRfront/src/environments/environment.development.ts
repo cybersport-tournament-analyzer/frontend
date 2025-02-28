a8fd2c7d264eacfea13042f2aefee2f84202258b
Ulquiorra <rodiontrig@gmail.com>
2025-02-28 21:16:14 +0300

    add Dockerfile, nginx.conf and env

--- VKRfront/src/environments/environment.development.ts
+++ VKRfront/src/environments/environment.development.ts
@@ -1,9 +1,7 @@
 export const environment : { authSource: string; matchSource: string } = {
 
-  // authSource: 'http://localhost:8080',
-  // matchSource: 'http://localhost:8081',
-  authSource: 'http://109.172.95.212:8080',
-  matchSource: 'http://109.172.95.212:8081',
+  authSource: (window as any).env?.authSource || "defaultauthSource",
+  matchSource: (window as any).env?.matchSource || "defaultmatchSource",
 
 
 };
