492eb5a10975aaa8bc5bb0dc65c4a83db005a201
Ulquiorra <rodiontrig@gmail.com>
2025-05-01 21:18:23 +0300

    add env config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,9 @@
-export const environment : { authSource: string; matchSource: string } = {
+export const environment : { authSource: string; matchSource: string;tournamentSource:string } = {
 
 
   authSource: (window as any).env?.authSource || "defaultauthSource",
   matchSource: (window as any).env?.matchSource || "defaultmatchSource",
-  tournamentSource: (window as any).env?.matchSource || "defaultTournamentSource",
+  tournamentSource: (window as any).env?.tournamentSource || "defaultTournamentSource",
 
 
 
