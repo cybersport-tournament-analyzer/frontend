f2b06e49f0209a11788ddc7b147f58044c0838d9
Ulquiorra <rodiontrig@gmail.com>
2025-05-05 21:49:51 +0300

    fix URL

--- public/env.template.js
+++ public/env.template.js
@@ -5,6 +5,7 @@
   window["env"]["authSource"] = "${API_AUTH_URL}";
   window["env"]["matchSource"] = "${API_MATCHMAKING_URL}";
   window["env"]["tournamentSource"] = "${API_TOURNAMENT_URL}";
+  window["env"]["userSource"] = "${API_USER_URL}";
 
 
 
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,10 @@
-export const environment : { authSource: string; matchSource: string;tournamentSource:string } = {
+export const environment : { authSource: string; matchSource: string;tournamentSource:string;userSource:string } = {
 
 
   authSource: (window as any).env?.authSource || "https://cybersport-tournament-analyzer-auth-service-cb72.twc1.net",
   matchSource: (window as any).env?.matchSource || "http://77.221.158.197:8081",
   tournamentSource: (window as any).env?.tournamentSource || "http://77.221.158.197:8082",
+  userSource: (window as any).env?.userSource ||  "http://77.221.158.197:8080",
 
 
 
