1318bf85b0607ecd2e6972042b13739c6ddf1c2b
Ulquiorra <rodiontrig@gmail.com>
2025-05-09 21:32:20 +0300

    add  game stats in MatchPAge

--- src/app/pages/match-page/match-page.component.ts
+++ src/app/pages/match-page/match-page.component.ts
@@ -4,7 +4,7 @@ import {TagComponent} from '../../componets/globals/tag/tag.component';
 import {TabsComponent} from '../../features/tabs/tabs.component';
 import {StageComponent} from '../../componets/stage/stage.component';
 import {TabDirective} from '../../features/tabs/tab.directive';
-import {JsonPipe, NgClass} from '@angular/common';
+import {JsonPipe, NgClass, NgForOf, NgIf} from '@angular/common';
 import {InfoMatchBlockComponent} from '../../componets/info-match-block/info-match-block.component';
 import {TableComponent} from '../../features/table/table.component';
 import {
@@ -22,6 +22,11 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {ButtonComponent} from '../../componets/globals/button/button.component';
 import {InfoCardComponent} from '../../componets/info-card/info-card.component';
 import {SpinnerComponent} from '../../features/spinner/spinner.component';
+import {ComparePlayersComponent} from '../../features/compare-players/compare-players.component';
+import {IdToNicknamePipe} from '../../pipes/id-to-nickname.pipe';
+import {
+  InfoSeriesBlockWithCompareComponent
+} from '../../componets/info-series-block-with-compare/info-series-block-with-compare.component';
 
 
 @Component({
@@ -45,7 +50,12 @@ import {SpinnerComponent} from '../../features/spinner/spinner.component';
     StatsResultDirective,
     ButtonComponent,
     InfoCardComponent,
-    SpinnerComponent
+    SpinnerComponent,
+    ComparePlayersComponent,
+    NgIf,
+    NgForOf,
+    IdToNicknamePipe,
+    InfoSeriesBlockWithCompareComponent
   ],
   templateUrl: './match-page.component.html',
   standalone: true,
@@ -78,6 +88,7 @@ export class MatchPageComponent implements OnInit{
   stages:any[]=['Общая информация','Статистика']
   selectedStage:string='Общая информация'
 
+  expendedStatsIsOpen:boolean = false
   selectState(state: any) {
     this.selectedStage=state
 
@@ -140,28 +151,102 @@ export class MatchPageComponent implements OnInit{
   //     KAST:95,
   //   }
   // ]
+  playerIds: string[] = [];
+
+  matrixData: any[] = [];
+
+  config:any ={
+    "1":'FischeR_ts33', "2":'lolpik33', "3":'swoow1', "4":'sh1ro', "5":'dexie',
+    "6":'why_n0t', "7":'nex1', "8":'relo', "9":'somuL', "10":'plaaaayer',
+  }
 
   baseStatsInMatches:WritableSignal<any>= signal<any>([])
-  constructor(private matchService:MatchService, private route:ActivatedRoute) {
+  constructor(private matchService:MatchService, private route:ActivatedRoute, private router:Router) {
   }
   public seriesData: WritableSignal<any> = signal<any>(null)
   public matches: WritableSignal<any> = signal<any>(null)
+  public comparePlayers: WritableSignal<any> = signal<any>(null)
   ngOnInit(): void {
 
-    this.matchService.getInfoSeries(this.route.snapshot.paramMap.get('id')!).subscribe((data:any)=>{
-      this.seriesData.set({...data,
-      format:data.pickBanSession.format})
+    this.matchService.getInfoSeries(this.route.snapshot.paramMap.get('id')!).subscribe({next:(data:any)=> {
+      if (data) {
+      this.seriesData.set({
+        ...data,
+        format: data.pickBanSession?.format
+      })
       this.matches.set(Object.values(this.seriesData()?.matches))
       console.log(Object.values(this.seriesData()?.matches))
       this.baseStatsInMatches.set((this.preparareTeamBaseStat(this.matches())))
       console.log(this.baseStatsInMatches())
-    })
+    }
+    },
+      error:()=>{
+      this.router.navigate(['/home'])
+    }
+      }
+    )
+
+    this.preparareDuelsMatrix()
+
+
+
+
+
+
+
+
+
+
+
+
+
   }
+  preparareDuelsMatrix(){
+
+    const team1Ids = Array.from(new Set(this.testDuels.map(d => d.player1Id.toString())));
+    const team2Ids = Array.from(new Set(this.testDuels.map(d => d.player2Id.toString())));
+    this.playerIds = [...team1Ids, ...team2Ids];
+
+    this.matrixData = this.playerIds.map(killerId => {
+      const row: any = {
+        killerId
+      };
+
+      this.playerIds.forEach(victimId => {
+        if (killerId === victimId) {
+          row[victimId] = null;
+          return;
+        }
+
+        const duel = this.testDuels.find(d =>
+          (d.player1Id.toString() === killerId && d.player2Id.toString() === victimId) ||
+          (d.player1Id.toString() === victimId && d.player2Id.toString() === killerId)
+        );
+
+        if (!duel) {
+          row[victimId] = null;
+          return;
+        }
+
+        const isKillerPlayer1 = duel.player1Id.toString() === killerId;
+        const kills = isKillerPlayer1 ? duel.player1Kills : duel.player2Kills;
+        const percent = isKillerPlayer1 ? duel.player1KillsPercent : duel.player2KillsPercent;
+
+        row[victimId] = { kills, percent };
+      });
+
+      return row;
+    });
+
+  }
+
   preparareTeamBaseStat(matches :any[]): any{
+
+    const team1Name =this.seriesData().team1Name
+    const team2Name =this.seriesData().team2Name
     const pullTeam1 = Object.values(this.seriesData().team1).map((item:any)=>{ return item.playerUsername})
     const pullTeam2 = Object.values(this.seriesData().team2).map((item:any)=>{ return item.playerUsername})
-    console.log(pullTeam1)
-    console.log(pullTeam2)
+    console.log(this.seriesData())
     console.log("preparareTeamBaseStat")
     console.log(matches)
     const statTeam1InAllMatches= matches!.map((item:any)=>{
@@ -184,7 +269,9 @@ export class MatchPageComponent implements OnInit{
     for (let i = 0; i < matches.length; i++) {
       statTeamsInMatches.push(
         {team1:statTeam1InAllMatches[i],
-          team2:statTeam2InAllMatches[i]
+          team2:statTeam2InAllMatches[i],
+          team1Score: matches[i].match.team1.name == team1Name? matches[i].match.team1.stats.score : matches[i].match.team2.stats.score,
+          team2Score: matches[i].match.team2.name == team2Name? matches[i].match.team2.stats.score : matches[i].match.team1.stats.score,
         }
       )
     }
@@ -208,4 +295,67 @@ export class MatchPageComponent implements OnInit{
     if (count === 0) return 0;
     return (cardHeight * count) + (gap * (count - 1)) + padding*2;
   }
+
+
+  openExtendedStats() {
+    this.expendedStatsIsOpen= !this.expendedStatsIsOpen
+  }
+
+
+  testDuels = [
+    { player1Id: "1", player2Id: "6", nickname1: 'FischeR_ts33', nickname2: 'why_n0t', player1Kills: 3, player2Kills: 2, player1KillsPercent: 60, player2KillsPercent: 40, index: 1 },
+    { player1Id: "1", player2Id: "7", nickname1: 'FischeR_ts33', nickname2: 'nex1', player1Kills: 1, player2Kills: 4, player1KillsPercent: 20, player2KillsPercent: 80, index: 2 },
+    { player1Id: "1", player2Id: "8", nickname1: 'FischeR_ts33', nickname2: 'relo', player1Kills: 5, player2Kills: 3, player1KillsPercent: 63, player2KillsPercent: 37, index: 3 },
+    { player1Id: "1", player2Id: "9", nickname1: 'FischeR_ts33', nickname2: 'somuL', player1Kills: 2, player2Kills: 2, player1KillsPercent: 50, player2KillsPercent: 50, index: 4 },
+    { player1Id: "1", player2Id: "10", nickname1: 'FischeR_ts33', nickname2: 'plaaaayer', player1Kills: 3, player2Kills: 4, player1KillsPercent: 43, player2KillsPercent: 57, index: 5 },
+
+    { player1Id: "2", player2Id: "6", nickname1: 'lolpik33', nickname2: 'why_n0t', player1Kills: 4, player2Kills: 1, player1KillsPercent: 80, player2KillsPercent: 20, index: 6 },
+    { player1Id: "2", player2Id: "7", nickname1: 'lolpik33', nickname2: 'nex1', player1Kills: 0, player2Kills: 5, player1KillsPercent: 0, player2KillsPercent: 100, index: 7 },
+    { player1Id: "2", player2Id: "8", nickname1: 'lolpik33', nickname2: 'relo', player1Kills: 2, player2Kills: 2, player1KillsPercent: 50, player2KillsPercent: 50, index: 8 },
+    { player1Id: "2", player2Id: "9", nickname1: 'lolpik33', nickname2: 'somuL', player1Kills: 3, player2Kills: 3, player1KillsPercent: 50, player2KillsPercent: 50, index: 9 },
+    { player1Id: "2", player2Id: "10", nickname1: 'lolpik33', nickname2: 'plaaaayer', player1Kills: 5, player2Kills: 0, player1KillsPercent: 100, player2KillsPercent: 0, index: 10 },
+
+    { player1Id: "3", player2Id: "6", nickname1: 'swoow1', nickname2: 'why_n0t', player1Kills: 2, player2Kills: 3, player1KillsPercent: 40, player2KillsPercent: 60, index: 11 },
+    { player1Id: "3", player2Id: "7", nickname1: 'swoow1', nickname2: 'nex1', player1Kills: 1, player2Kills: 1, player1KillsPercent: 50, player2KillsPercent: 50, index: 12 },
+    { player1Id: "3", player2Id: "8", nickname1: 'swoow1', nickname2: 'relo', player1Kills: 4, player2Kills: 1, player1KillsPercent: 80, player2KillsPercent: 20, index: 13 },
+    { player1Id: "3", player2Id: "9", nickname1: 'swoow1', nickname2: 'somuL', player1Kills: 3, player2Kills: 3, player1KillsPercent: 50, player2KillsPercent: 50, index: 14 },
+    { player1Id: "3", player2Id: "10", nickname1: 'swoow1', nickname2: 'plaaaayer', player1Kills: 5, player2Kills: 2, player1KillsPercent: 71, player2KillsPercent: 29, index: 15 },
+
+    { player1Id: "4", player2Id: "6", nickname1: 'sh1ro', nickname2: 'why_n0t', player1Kills: 1, player2Kills: 2, player1KillsPercent: 33, player2KillsPercent: 67, index: 16 },
+    { player1Id: "4", player2Id: "7", nickname1: 'sh1ro', nickname2: 'nex1', player1Kills: 3, player2Kills: 0, player1KillsPercent: 100, player2KillsPercent: 0, index: 17 },
+    { player1Id: "4", player2Id: "8", nickname1: 'sh1ro', nickname2: 'relo', player1Kills: 2, player2Kills: 4, player1KillsPercent: 33, player2KillsPercent: 67, index: 18 },
+    { player1Id: "4", player2Id: "9", nickname1: 'sh1ro', nickname2: 'somuL', player1Kills: 1, player2Kills: 1, player1KillsPercent: 50, player2KillsPercent: 50, index: 19 },
+    { player1Id: "4", player2Id: "10", nickname1: 'sh1ro', nickname2: 'plaaaayer', player1Kills: 2, player2Kills: 3, player1KillsPercent: 40, player2KillsPercent: 60, index: 20 },
+
+    { player1Id: "5", player2Id: "6", nickname1: 'dexie', nickname2: 'why_n0t', player1Kills: 3, player2Kills: 1, player1KillsPercent: 75, player2KillsPercent: 25, index: 21 },
+    { player1Id: "5", player2Id: "7", nickname1: 'dexie', nickname2: 'nex1', player1Kills: 2, player2Kills: 2, player1KillsPercent: 50, player2KillsPercent: 50, index: 22 },
+    { player1Id: "5", player2Id: "8", nickname1: 'dexie', nickname2: 'relo', player1Kills: 0, player2Kills: 4, player1KillsPercent: 0, player2KillsPercent: 100, index: 23 },
+    { player1Id: "5", player2Id: "9", nickname1: 'dexie', nickname2: 'somuL', player1Kills: 5, player2Kills: 5, player1KillsPercent: 50, player2KillsPercent: 50, index: 24 },
+    { player1Id: "5", player2Id: "10", nickname1: 'dexie', nickname2: 'plaaaayer', player1Kills: 4, player2Kills: 3, player1KillsPercent: 57, player2KillsPercent: 43, index: 25 }
+  ];
+
+
+
+  selectedUsers:any[]=[]
+  selectedUsersId:any[]=[]
+  isSelected(id:string){
+return this.selectedUsersId.includes(id)
+  }
+  selectUser(user:any,event:any) {
+
+      if (this.selectedUsers[0]?.steam_id_64 != user.steam_id_64 && this.selectedUsers[1]?.steam_id_64 != user.steam_id_64){
+        this.selectedUsers.push({
+          steam_id_64:user.steam_id_64,
+          nickname_override : user.nickname_override
+        })
+        this.selectedUsersId.push(user.steam_id_64)
+      }else{
+        this.selectedUsers=this.selectedUsers.filter((item:any)=>item.steam_id_64 != user.steam_id_64)
+        this.selectedUsersId=this.selectedUsersId.filter((item:any)=>item != user.steam_id_64)
+      }
+
+    console.log(this.selectedUsers)
+  }
+
+  protected readonly String = String;
 }
