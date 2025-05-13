8de3f7288bcd94cdb50bccda8345d36da21601af
Ulquiorra <rodiontrig@gmail.com>
2025-05-13 19:45:10 +0300

    fix small bugs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment : { authSource: string; matchSource: string;tournamentSource: string;userSource:string; statsSource:string } = {
 
 
-  authSource: (window as any).env?.authSource || "https://cybersport-tournament-analyzer-auth-service-cb72.twc1.net",
+  authSource: (window as any).env?.authSource || "https://cybersport-tournament-analyzer-auth-service-96fd.twc1.net",
   matchSource: (window as any).env?.matchSource || "http://77.221.158.197:8081",
   tournamentSource: (window as any).env?.tournamentSource || "http://77.221.158.197:8082",
   userSource: (window as any).env?.userSource ||  "http://77.221.158.197:8080",
